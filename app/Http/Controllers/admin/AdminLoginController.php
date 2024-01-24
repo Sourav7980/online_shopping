fc56301e3523c3da6c4add52f2b41e55253652d1
Sourav Ghosh <souravghosh007980@gmail.com>
2024-01-24 12:24:13 +0530

    validation

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -10,4 +10,19 @@ class AdminLoginController extends Controller
     public function index(){
         return view('admin.login');
     }
+
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+    ]);
+
+    if($validator->passes()){
+
+    }else{
+        return redirect()->route('admin.login')
+        ->withErrors($validator)
+        ->withInput($request->only('email'));
+    }
+}
 }
