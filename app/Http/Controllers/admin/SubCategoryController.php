1113a5d4664024f801f32cbaf64c206f00262091
Sushanta Mukherjee <mukherjeeanup720@gmail.com>
2024-03-19 18:40:32 +0530

    error fix

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -9,49 +9,52 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class SubCategoryController extends Controller
-{  
+{
+    public function index(Request $request)
+    {
+        $subCategories = SubCategory::select('sub_categories.*', 'categories.name as categoryName')
+            ->latest('id')
+            ->leftJoin('categories', 'categories.id', '=', 'sub_categories.category_id');
 
-    public function index(Request $request) {
-        $subCategories= SubCategory::select('sub_categories.*','categories.name as categoryName')
-                                    ->latest('id')
-                                    ->leftJoin('categories','categories.id','sub_categories.categories_id');
-        if(!empty($request->get('keyword'))){
-            $subCategories= $subCategories->where('name','like','%'.$request->get('keyword').'%');
+        if (!empty($request->get('keyword'))) {
+            $subCategories->where('sub_categories.name', 'like', '%' . $request->get('keyword') . '%');
         }
 
-        $subCategories= $subCategories-> paginate(10);
-        return view('admin.sub_category.list',compact('subCategories'));
-    }
-public function create() {
-        $categories = Category::orderBy('name','ASC')->get();
-        $data['categories'] = $categories;
-        return view('admin.sub_category.create',$data);
+        $subCategories = $subCategories->paginate(10);
+        return view('admin.sub_category.list', compact('subCategories'));
     }
 
-    public function store(Request $request) {
-        $validator = Validator::make($request->all(),[
+    public function create()
+    {
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $data['categories'] = $categories;
+        return view('admin.sub_category.create', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'slug' => 'required|unique:table:sub_categories',
+            'slug' => 'required|unique:sub_categories',
             'category' => 'required',
             'status' => 'required'
         ]);
 
-        if($validator->passes()) {
+        if ($validator->passes()) {
             $subCategory = new SubCategory();
             $subCategory->name = $request->name;
-            $subCategory->Slug = $request->name;
-            $subCategory->Status = $request->name;
-            $subCategory->category_id = $request->name;
+            $subCategory->slug = $request->slug;
+            $subCategory->status = $request->status;
+            $subCategory->category_id = $request->category;
             $subCategory->save();
 
-            $request->session()->flash('success','Sub Category create successfully.');
+            $request->session()->flash('success', 'Sub Category created successfully.');
 
             return response([
                 'status' => true,
-                'message' => 'Sub Category create successfully.'
+                'message' => 'Sub Category created successfully.'
             ]);
-
-        }else{
+        } else {
             return response([
                 'status' => false,
                 'errors' => $validator->errors()
